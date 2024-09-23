81f4a9c6ddcc0dad7f46ae8d569961d8e8899434
Arthur Campos <138174613+arthurd3@users.noreply.github.com>
2024-09-23 10:47:46 -0300

    Multiplos 5 (C#)

---
+++ Multiplos 5 a 100/Program.cs
@@ -0,0 +1,30 @@
+﻿//Elabore um programa em C# que mostre todos os números múltiplos de 5 de 100 até 0.
+
+
+
+
+
+using System;
+
+class Program{
+
+    public static void Main(string[] args)
+    {
+
+        int num = 5 , resultado = 0 , multiplo =0;
+        
+        while(resultado < 100){
+            resultado = num * multiplo;
+            Console.WriteLine($"Multiplos De 5 : {resultado}");
+            multiplo++;
+        }
+
+
+        for (int i = 100 ; i >= 0 ; i-= 5){
+            Console.WriteLine($"Multiplos De 5 : {i}");
+        }
+
+
+    }
+
+}
